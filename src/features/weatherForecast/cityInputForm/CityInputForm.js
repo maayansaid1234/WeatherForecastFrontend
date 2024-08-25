3389cf993c4dba79e356e124bcf9a092082197e6
maayansaid <maayansaid1234@gmail.com>
2024-08-25 14:07:24 +0300

    improve responsiveness

--- src/features/weatherForecast/cityInputForm/CityInputForm.js
+++ src/features/weatherForecast/cityInputForm/CityInputForm.js
@@ -1,35 +1,3 @@
-// import { useForm } from 'react-hook-form';
-// import "./form.css";
-
-// const Form = ({ onCheck }) => {
-//     const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onSubmit" });
-
-//     const onSubmit = (data) => {
-//         onCheck(data.city);
-//     };
-
-//     return (
-//         <form className="form" onSubmit={handleSubmit(onSubmit)}>
-//             <div className="formGroup">
-//                 <label className="formLabel">City name</label>
-//                 <div className="formInputContainer">
-//                     <input
-//                         className="formInput"
-//                         {...register('city', { required: "City name is required" })}
-//                         type="text"
-//                     />
-                   
-//                     <button type="submit" className="formSubmitButton">Check</button>
-//                 </div>
-//                 {errors.city && <span className='formErrorMessage'>{errors.city.message}</span>}
-                
-//             </div>
-//         </form>
-//     );
-// };
-
-// export default Form;
-
 import { useForm } from 'react-hook-form';
 import "./form.css";
 
